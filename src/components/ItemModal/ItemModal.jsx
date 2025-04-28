17a36dffc89a08955db1f48cd4beac842b401a76
FelixG105 <felix.gonzalez105@gmail.com>
2025-04-28 14:37:25 -0400

    ModalWithForm CSS

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -14,8 +14,8 @@ function ItemModal({ activeModal, onClose, card }) {
         </button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+          <h2 className="modal__footer-caption">{card.name}</h2>
+          <p className="modal__footer-weather">Weather: {card.weather}</p>
         </div>
       </div>
     </div>
