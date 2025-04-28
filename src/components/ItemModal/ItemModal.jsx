68b2ecf161a90152fc99f45be3cc1662924bd6f6
FelixG105 <felix.gonzalez105@gmail.com>
2025-04-28 11:27:00 -0400

    ItemModal Close Button

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,26 +1,25 @@
 import './ItemModal.css';
+import close from '../../assets/close.png';
 
-function ItemModal ({activeModal, onClose, card}) {
-    return (
-        <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
-            <div className="modal__content modal__content_type_image">
-                <button 
-                    onClick={onClose}
-                    type='button' 
-                    className="modal__close">CLOSE
-                </button>
-                <img 
-                    src={card.link} 
-                    alt="" 
-                    className="modal__image" />
-                <div className="modal__footer">
-                    <h2 className="modal__caption">{card.name}</h2>
-                    <p className="modal__weather">Weather: {card.weather}</p>
-                </div>
-            </div>
+function ItemModal({ activeModal, onClose, card }) {
+  return (
+    <div className={`modal ${activeModal === 'preview' && 'modal_opened'}`}>
+      <div className="modal__content modal__content_type_image">
+        <button
+          onClick={onClose}
+          type="button"
+          className="modal__close modal__close_type_preview"
+        >
+          <img src={close} alt="close-button" />
+        </button>
+        <img src={card.link} alt="" className="modal__image" />
+        <div className="modal__footer">
+          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__weather">Weather: {card.weather}</p>
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
 
 export default ItemModal;
